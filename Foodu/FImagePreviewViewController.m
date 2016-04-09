a4306fe7fc468af98aa4320e51f987d6f299cebe
AbbinVarghese <paadam2@gmail.com>
2016-04-09 19:06:28 +0530

    Revert "GPUImage added"
    This reverts commit 13be76a560aef7288885b86ff8ca8cfbf18d7c2e.

--- Foodu/FImagePreviewViewController.m
+++ Foodu/FImagePreviewViewController.m
@@ -9,13 +9,10 @@
 #import "FImagePreviewViewController.h"
 #import "FImagePreviewCollectionViewCell.h"
 #import "FEffectCollectionViewCell.h"
-@import GPUImage;
 
 @interface FImagePreviewViewController ()
 @property (weak, nonatomic) IBOutlet UICollectionView *photoCollectionView;
 @property (weak, nonatomic) IBOutlet UICollectionView *effectsCollectionView;
-
-@property (strong, nonatomic) NSMutableArray *filterArray;
 
 @end
 
@@ -23,8 +20,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.filterArray = [[NSMutableArray alloc]init];
     
     [self.photoCollectionView registerNib:[UINib nibWithNibName:@"FImagePreviewCollectionViewCell" bundle:[NSBundle mainBundle]] forCellWithReuseIdentifier:@"FImagePreviewCollectionViewCell"];
         [self.effectsCollectionView registerNib:[UINib nibWithNibName:@"FEffectCollectionViewCell" bundle:[NSBundle mainBundle]] forCellWithReuseIdentifier:@"FEffectCollectionViewCell"];
@@ -40,63 +35,12 @@
     return YES;
 }
 
--(void)viewDidAppear:(BOOL)animated{
-    [super viewDidAppear:animated];
-    for (int i = 0; i <= 10; i++) {
-        CGFloat scale = [UIScreen mainScreen].scale;
-        CGSize cellSize = ((UICollectionViewFlowLayout *)self.photoCollectionView.collectionViewLayout).itemSize;
-        CGSize AssetGridThumbnailSize = CGSizeMake(cellSize.width * scale, cellSize.height * scale);
-        [self.filterArray addObject:[self imageWithImage:[self.imageArray objectAtIndex:0] scaledToSize:AssetGridThumbnailSize]];
-    }
-    
-    for (int i = 0; i<self.filterArray.count; i++) {
-        [self.filterArray replaceObjectAtIndex:i withObject:[self filteredImageForIndex:i withOriginal:[self.filterArray objectAtIndex:i]]];
-    }
-    
-    [self.effectsCollectionView reloadData];
-}
-
--(UIImage*)filteredImageForIndex:(NSInteger)index withOriginal:(UIImage*)image{
-    switch (index) {
-        case 0:{
-            GPUImagePicture *stillImageSource = [[GPUImagePicture alloc] initWithImage:image];
-            
-            GPUImagePicture *lookupImageSource = [[GPUImagePicture alloc] initWithImage:[UIImage imageNamed:@"cloudy07.jpg"]];
-            
-            GPUImageLookupFilter *lookupFilter = [[GPUImageLookupFilter alloc] init]; 
-            [stillImageSource addTarget:lookupFilter];
-            [lookupImageSource addTarget:lookupFilter];
-            
-            [lookupFilter useNextFrameForImageCapture];
-            [stillImageSource processImage];
-            [lookupImageSource processImage];
-            
-            UIImage *filteredimage = [lookupFilter imageFromCurrentFramebuffer];
-            
-            return filteredimage;
-        }
-            break;
-            
-        default:
-            return image;
-            break;
-    }
-}
-
-- (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
-    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
-    [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
-    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return newImage;
-}
-
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
     if (collectionView.tag == 0) {
         return self.imageArray.count;
     }
     else{
-        return self.filterArray.count;
+        return 14;
     }
 }
 
@@ -109,7 +53,6 @@
     }
     else{
         FEffectCollectionViewCell * effCell = [collectionView dequeueReusableCellWithReuseIdentifier:@"FEffectCollectionViewCell" forIndexPath:indexPath];
-        effCell.imageView.image = [self.filterArray objectAtIndex:indexPath.row];
         return effCell;
     }
 }
